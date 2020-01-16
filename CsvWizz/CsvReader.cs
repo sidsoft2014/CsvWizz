d690cd24a4bdb19f0ac9e11dca528e2a42ab72b2
William Parker <william.parker@first4lawyers.com>
2020-01-16 11:43:14 +0000

    Add project files.

---
+++ CsvWizz/CsvReader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CsvWizz
+{
+	public class CsvReader<T>
+	{
+		/// <summary>
+		/// Reads a .csv file into JSON format
+		/// </summary>
+		/// <param name="path">The path to the .csv file.</param>
+		/// <param name="delimiter">The string to use as the value delimiter. (default ",")</param>
+		/// <returns></returns>
+		public static string ReadToJSON(string path, string delimiter = ",")
+		{
+			// Can't parse a file that doesn't exist
+			if (!File.Exists(path))
+				return null;
+
+			// Read text as an array to save splitting on new lines
+			var csvText = File.ReadAllLines(path);
+
+			// If .csv includes headers they should be the first row.
+			var propNames = SplitRow(csvText[0], delimiter);
+
+			// Convert .csv values into JSON objects and add to collection
+			var objStrings = new List<string>();
+			for (var r = 1; r < csvText.Length; r++)
+			{
+				var row = csvText[r];
+				var values = SplitRow(row, delimiter);
+				var objProps = new List<string>();
+				for (int i = 0; i < propNames.Length; i++)
+				{
+					var name = propNames[i];
+					var value = values[i];
+					objProps.Add($"\"{name}\": \"{value}\"");
+
+				}
+
+				var valsSb = new StringBuilder();
+				valsSb.AppendLine("{");
+				valsSb.AppendLine(string.Join(",\r\n", objProps));
+				valsSb.AppendLine("}");
+				objStrings.Add(valsSb.ToString());
+			}
+
+			// Join all objects with comma and wrap in square brackets to create JSON array
+			var sb = new StringBuilder();
+			sb.AppendLine("[");
+			sb.Append(string.Join(",\r\n", objStrings));
+			sb.AppendLine("]");
+
+			return sb.ToString();
+		}
+
+		public static T[] ReadToObject(string path, string delimiter = ",")
+		{
+			var json = ReadToJSON(path, delimiter);
+			var deserialised = JsonConvert.DeserializeObject<T[]>(json);
+			return deserialised;
+		}
+
+		private static string[] SplitRow(string row, string delimiter)
+		{
+			// Split on delimiter, don't remove empty entries, trim results
+			return row.Split(new string[] { delimiter }, StringSplitOptions.None)
+				.Select(p => p.Trim()).ToArray();
+		}
+	}
+}
---
+++ CsvWizz/CsvWriter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CsvWizz
+{
+	public class CsvWriter
+	{
+		/// <summary>
+		/// Writes a collection of objects to a .csv file. Optinaly appending to or overwriting any existing file.
+		/// </summary>
+		/// <param name="rows">Object collection to write to the csv</param>
+		/// <param name="path">Path of the .csv file</param>
+		/// <param name="appendIfFileExists">Append data if the file already exists. Otherwise overwrite. (default true)</param>
+		/// <param name="delimiter">The string to use as a delimiter. (default ",")</param>
+		/// <param name="includeHeaders">Wether to include property names as headers. (default true)
+		/// <para>Will be ignored if 'appendIfFileExists' is true and the file exists.</para></param>
+		/// <returns></returns>
+		public static bool Write(IEnumerable<object> rows, string path, bool appendIfFileExists = true, string delimiter = ",", bool includeHeaders = true)
+		{
+			// Quit here if no data provided
+			if (rows?.Any() == false)
+				return false;
+
+			var sb = new StringBuilder();
+
+			// Add headers if required
+			if (includeHeaders)
+			{
+				try
+				{
+					/*
+					 * Nobody wants headers in the middle of their dataset.
+					 * If the file exists and we are told to append data we need to skip adding headers.
+					 */
+					if (!File.Exists(path) || !appendIfFileExists)
+					{
+						var headers = string.Join(delimiter, rows.First().GetType().GetProperties().Select(p => p.Name));
+						sb.AppendLine(headers);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Parsing headers failed! {ex.Message}");
+					return false;
+				}
+			}
+
+			// Get row values
+			try
+			{
+				foreach (var row in rows)
+				{
+					var values = row.GetType().GetProperties().Select(p => p.GetValue(row)?.ToString())?.ToArray();
+					sb.AppendLine(string.Join(delimiter, values));
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Parsing rows failed!: {ex.Message}");
+				return false;
+			}
+
+			// Create final string and write or append to file
+			var csvText = sb.ToString();
+			try
+			{
+				if (!appendIfFileExists)
+				{
+					File.WriteAllText(path, csvText);
+				}
+				else
+				{
+					File.AppendAllText(path, csvText);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Writing csv failed! {ex.Message}");
+				return false;
+			}
+
+			// Yay, we succeeded!
+			return true;
+		}
+	}
+}
